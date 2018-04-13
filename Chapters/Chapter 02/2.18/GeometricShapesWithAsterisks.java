9b79ea8b1e5543c450d59b6928fe4d01b44658ac
Hianuy <30296650+Hianuy@users.noreply.github.com>
2018-04-12 23:21:37 -0300

    Organization and Cleaning

---
+++ Chapters/Chapter 02/2.18/GeometricShapesWithAsterisks.java
@@ -0,0 +1,38 @@
+/*
+ 	Filename : GeometricShapesWithAsterisks.java
+ 
+ 	Descrition :
+ 			(2.18)//Write a program that displays a rectangle, an oval, an arrow, and a diamond
+	
+	Created: 12/04/18 23:10:20
+	Revision:none
+	
+	Author:Hianuy Esperidião - hianuy2@gmail.com
+	Version 1.0:
+	
+	 
+	 
+	 
+*/
+ 
+class GeometricShapesWithAsterisksJava {
+
+	  public static void main(String[] args) {
+        System.out.println("**********      ***      *         *      ");
+        System.out.println("*        *     *   *    ***       * *     ");
+        System.out.println("*        *    *     *  *****     *   *   " );
+        System.out.println("*        *    *      * 	 *      *     *   ");
+        System.out.println("*        *    *      *   *     *       *  ");
+        System.out.println("*        *    *      *   *      *     *   ");
+        System.out.println("*        *    *      *   *       *   *    ");
+        System.out.println("*        *     *    *    *        * *     ");
+        System.out.println("**********       ***     *         *      ");
+       
+		
+		
+		
+		
+		
+	    }
+
+}
