696ffce41f730db74fd454b25942230d61764e6e
Hianuy <30296650+Hianuy@users.noreply.github.com>
2018-04-12 22:55:26 -0300

    Organization and Cleaning

---
+++ Chapters/Chapter 02/2.16/Comparation.java
@@ -0,0 +1,53 @@
+/*
+* 	Filename :Comparation.java
+* 
+* 	Descrition :
+*
+*	
+*	Created: 12/04/18 22:51:01
+*
+*	Revision:none
+*	(2.16) write a program that compares two numbers and says which is the largest or which is the smallest or if the numbers are
+*	Author:Hianuy Esperidião - hianuy2@gmail.com
+*	Version 1.0:
+*	
+*	 
+*	 
+* ============================================================================================	 
+*/
+
+
+import java.util.*;
+class Comparation {
+//(2.16) Write a program that compares two numbers and says which is the largest or which is the smallest or if the numbers are equals
+
+	public static void main(String[] args) {
+	
+			System.out.println("Enter the fist number:");
+			System.out.println("Enter the second number:");
+			
+			Scanner in = new Scanner(System.in);
+			
+			int num1 = in.nextInt();
+			
+			int num2 = in.nextInt();
+		
+			
+			if(num1>num2) {
+				System.out.println("the first number is greater than the second number");
+				
+			}	else { 
+						if(num1==num2) {
+							  System.out.println("the numbers are the same");
+							
+			
+				
+						 }else {
+								   if(num1<num2) {
+		        		                 System.out.println("the first number is smaller than the second number");
+		        	        	    }
+					           }		
+
+					  }
+	 }
+}
