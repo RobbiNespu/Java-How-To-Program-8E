6441a4921343ced176ee3286fc8b777b2891c6ed
Hianuy <30296650+Hianuy@users.noreply.github.com>
2018-04-12 22:41:36 -0300

    Create Output_object.java

---
+++ Chapters/Chapter 02/2.14/Output_object.java
@@ -0,0 +1,46 @@
+/*
+ 	Filename : output_object.java 
+ 
+ 	Descrition :
+ 			(2.14)Write an application that displays numbers from 1 to 4 on the same line, with each pair of adjacent numbers separated by a space.
+	  		Write the program using the following techniques.
+	
+	Created: 12/04/18
+
+	Revision:none
+	
+	Author:Hianuy Esperidião - hianuy2@gmail.com
+	Version 1.0:
+	
+	 
+	 
+	 
+*/
+
+
+
+class output_object {
+	
+	
+		public static void main(String[] args) {
+/*	1.0 */	 System.out.println("1 2 3 4 ");
+		
+	    
+/*	1.1 */ 	  System.out.print("1");
+			  System.out.print(" ");
+			  System.out.print("2");
+			  System.out.print(" ");
+			  System.out.print("3");
+			  System.out.print(" ");
+			  System.out.print("4\n");
+		  
+ /*	1.2 */	  System.out.printf("1 2 3 4" );
+		
+	
+		}
+
+	
+
+}
+
+
