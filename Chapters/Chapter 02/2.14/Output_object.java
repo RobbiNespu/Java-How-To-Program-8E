ff55477881f0cae34c76f02188d17652b219a98b
Hianuy <30296650+Hianuy@users.noreply.github.com>
2018-04-12 23:25:25 -0300

    Update Output_object.java

--- Chapters/Chapter 02/2.14/Output_object.java
+++ Chapters/Chapter 02/2.14/Output_object.java
@@ -23,16 +23,16 @@ class output_object {
 	
 	
 		public static void main(String[] args) {
-/*	1.0 */	 System.out.println("1 2 3 4 ");
+/*	1.0 */	  System.out.println("1 2 3 4 ");
 		
 	    
 /*	1.1 */ 	  System.out.print("1");
-			  System.out.print(" ");
-			  System.out.print("2");
-			  System.out.print(" ");
-			  System.out.print("3");
-			  System.out.print(" ");
-			  System.out.print("4\n");
+		  System.out.print(" ");
+		  System.out.print("2");
+		  System.out.print(" ");
+		  System.out.print("3");
+		  System.out.print(" ");
+		  System.out.print("4\n");	  
 		  
  /*	1.2 */	  System.out.printf("1 2 3 4" );
 		
