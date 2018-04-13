c696cf9fced8f66a580e76bc9d77fb7ebf4922e9
Hianuy <30296650+Hianuy@users.noreply.github.com>
2018-04-12 23:23:34 -0300

    Update GeometricShapesWithAsterisks.java

--- Chapters/Chapter 02/2.18/GeometricShapesWithAsterisks.java
+++ Chapters/Chapter 02/2.18/GeometricShapesWithAsterisks.java
@@ -2,7 +2,7 @@
  	Filename : GeometricShapesWithAsterisks.java
  
  	Descrition :
- 			(2.18)//Write a program that displays a rectangle, an oval, an arrow, and a diamond
+ 	(2.18)//Write a program that displays a rectangle, an oval, an arrow, and a diamond
 	
 	Created: 12/04/18 23:10:20
 	Revision:none
@@ -18,15 +18,15 @@
 class GeometricShapesWithAsterisksJava {
 
 	  public static void main(String[] args) {
-        System.out.println("**********      ***      *         *      ");
-        System.out.println("*        *     *   *    ***       * *     ");
-        System.out.println("*        *    *     *  *****     *   *   " );
-        System.out.println("*        *    *      * 	 *      *     *   ");
-        System.out.println("*        *    *      *   *     *       *  ");
-        System.out.println("*        *    *      *   *      *     *   ");
-        System.out.println("*        *    *      *   *       *   *    ");
-        System.out.println("*        *     *    *    *        * *     ");
-        System.out.println("**********       ***     *         *      ");
+        System.out.println("**********      ***      *         *     ");
+        System.out.println("*        *     *   *    ***       * *    ");
+        System.out.println("*        *    *     *  *****     *   *   ");
+        System.out.println("*        *    *      *   *      *     *  ");
+        System.out.println("*        *    *      *   *     *       * ");
+        System.out.println("*        *    *      *   *      *     *  ");
+        System.out.println("*        *    *      *   *       *   *   ");
+        System.out.println("*        *     *    *    *        * *    ");
+        System.out.println("**********       ***     *         *     ");
        
 		
 		
