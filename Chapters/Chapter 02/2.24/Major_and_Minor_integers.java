c2f284f94181c95c7e7d4fddbbcc6ba284ead398
Hianuy <30296650+Hianuy@users.noreply.github.com>
2018-04-16 17:30:11 -0300

    Organization and Cleaning

--- Chapters/Chapter 02/2.24/Major_and_Minor_integers.java
+++ Chapters/Chapter 02/2.24/Major_and_Minor_integers.java
@@ -24,47 +24,47 @@ import java.util.*;
 class Major_and_Minor_integers {
 
 	public static void main(String[] args) {
-		 double HigherNumber = 0;
-		 double smallerNumber = 0;
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter the first number:");
-        System.out.println("Enter the second number:");
-        System.out.println("Enter the third number:");
-        System.out.println("Enter the fourth number:");
-        System.out.println("Enter the fifth number:");
+		double higherNumber = 0;
+		double smallerNumber = 0;
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the first number:");
+		System.out.println("Enter the second number:");
+		System.out.println("Enter the third number:");
+		System.out.println("Enter the fourth number:");
+		System.out.println("Enter the fifth number:");
 
-        double num1 = in.nextInt();
-        double num2 = in.nextInt();
-        double num3 = in.nextInt();
-        double num4 = in.nextInt();
-        double num5 = in.nextInt();
+		double num1 = in.nextInt();
+		double num2 = in.nextInt();
+		double num3 = in.nextInt();
+		double num4 = in.nextInt();
+		double num5 = in.nextInt();
 
-      
-        if (num1 > num2 && num1 > num3 && num1 > num4 && num1> num5) {
-        	HigherNumber= num1;
+
+		if (num1 > num2 && num1 > num3 && num1 > num4 && num1> num5) {
+			higherNumber = num1;
 		} else if (num2 > num1 && num2 > num3 && num2 > num4 && num2 > num5) {
-			HigherNumber = num2;
+			higherNumber = num2;
 		} else if (num3 > num1 && num3 > num2 && num3 > num4 && num3 > num5) {
-			HigherNumber= num3;
+			higherNumber = num3;
 		}else if (num4 > num1 && num4 > num2 && num4 > num3 && num4 > num5) {
-				HigherNumber= num4;
+			higherNumber = num4;
 		}else if (num5 > num1 && num5 > num2 && num5 > num3 && num5 > num4) {
-			HigherNumber= num5;
+			higherNumber = num5;
 		}	
 
-        if (num1 < num2 && num1 < num3 && num1 < num4 && num1 < num5) {
-        	smallerNumber= num1;
+		if (num1 < num2 && num1 < num3 && num1 < num4 && num1 < num5) {
+			smallerNumber = num1;
 		} else if (num2 < num1 && num2 < num3 && num2 < num4 && num2 < num5) {
 			smallerNumber = num2;
 		} else if (num3 < num1 && num3 < num2 && num3 < num4 && num3 < num5) {
-			smallerNumber= num3;
+			smallerNumber = num3;
 		}else if (num4 < num1 && num4 < num2 && num4 < num3 && num4 < num5) {
-			smallerNumber= num4;
+			smallerNumber = num4;
 		}else if (num5 < num1 && num5 < num2 && num5 < num3 && num5 < num4) {
-			smallerNumber= num5;
-		}
-  
-        System.out.println("Higher number : " + HigherNumber);
-		    System.out.println("Smaller Número : " + smallerNumber);
+			smallerNumber = num5;
+			}
+
+		System.out.println("Higher number : " + higherNumber
+		System.out.println("Smaller Número : " + smallerNumber);
 	}
 }
