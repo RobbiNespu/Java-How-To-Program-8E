a6a96f31addeab80822cd73a289aeb62f56eb7b5
Hianuy <30296650+Hianuy@users.noreply.github.com>
2018-04-16 15:08:47 -0300

    Create WhatTheImpress3.java

---
+++ Chapters/Chapter 02/2.21/WhatTheImpress3.java
@@ -0,0 +1,18 @@
+*
+* 	Filename :WhatTheImpress3.java
+* 
+* 	Descrition : (2.21) What does the following code print?
+*	
+* 			
+*	
+*	Created: 16/04/18 14:56:32
+*
+*	Revision:none
+*	
+*	Author:Hianuy Esperidião - hianuy2@gmail.com
+*	Version 1.0:
+*	
+*	 
+*	 
+* ============================================================================================	 
+*/
