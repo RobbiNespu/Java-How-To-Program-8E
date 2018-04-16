a77d918f0017d9905a678faa9221c9162bd52913
Hianuy <30296650+Hianuy@users.noreply.github.com>
2018-04-16 15:26:29 -0300

    Organization and Cleaning

--- Chapters/Chapter 02/2.23/WhatTheImpress5.java
+++ Chapters/Chapter 02/2.23/WhatTheImpress5.java
@@ -17,13 +17,13 @@
 * ============================================================================================	 
 
 
-class whattheimpress5 {
+class WhatTheImpress5 {
 
 	  public static void main(String[] args) {
 		
 		     System.out.printf("%s\n%s\n%s\n","*","***","*****");		//  *	
-																	                              // ***
-																	                              // *****
+											// ***						                              // ***
+											// *****						                              // *****
 		
 		
 	  }
