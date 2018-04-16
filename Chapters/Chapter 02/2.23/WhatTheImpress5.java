3f877e6f3a7254517aa0b46179663444c930136a
Hianuy <30296650+Hianuy@users.noreply.github.com>
2018-04-16 15:25:27 -0300

    Organization and Cleaning

---
+++ Chapters/Chapter 02/2.23/WhatTheImpress5.java
@@ -0,0 +1,31 @@
+*
+* 	Filename :WhatTheImpress5.java
+* 
+* 	Descrition : (2.23) What does the following code print?
+*	
+* 			
+*	
+*	Created: 16/04/18 15:21:38
+*
+*	Revision:none
+*	
+*	Author:Hianuy Esperidião - hianuy2@gmail.com
+*	Version 1.0:
+*	
+*	 
+*	 
+* ============================================================================================	 
+
+
+class whattheimpress5 {
+
+	  public static void main(String[] args) {
+		
+		     System.out.printf("%s\n%s\n%s\n","*","***","*****");		//  *	
+																	                              // ***
+																	                              // *****
+		
+		
+	  }
+
+}
