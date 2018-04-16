2c85cb5b2f56396e6a9a29a270c0d92183fc4965
Hianuy <30296650+Hianuy@users.noreply.github.com>
2018-04-16 15:17:35 -0300

    Organization and Cleaning

--- Chapters/Chapter 02/2.22/WhatTheImpress4.java
+++ Chapters/Chapter 02/2.22/WhatTheImpress4.java
@@ -23,7 +23,7 @@ class WhatTheImpress4 {
 	public static void main(String[] args) {
 		
       System.out.print("*");			
-      System.out.println("***");	    // ****
+      System.out.println("***");	// ****
       System.out.println("*****");	// *****	
       System.out.print("****");		// ******
       System.out.println("**");		
