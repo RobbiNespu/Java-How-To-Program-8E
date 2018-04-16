acd94520cd872c16a84a2d8f97793bc3c4f71362
Hianuy <30296650+Hianuy@users.noreply.github.com>
2018-04-16 14:53:28 -0300

    Create WhatTheImpress.java

---
+++ Chapters/Chapter 02/2.19/WhatTheImpress.java
@@ -0,0 +1,35 @@
+*
+* 	Filename :WhatTheImpress.java
+* 
+* 	Descrition :
+* 			(2.15)write an application that prompts the user to insert two integers, obtains the user from these numbers and prints their sum, difference, product, quotient.
+*	
+*	Created: 12/04/18 22:44:42
+*
+*	Revision:none
+*	
+*	Author:Hianuy Esperidião - hianuy2@gmail.com
+*	Version 1.0:
+*	
+*	 
+*	 
+* ============================================================================================	 
+*/
+
+
+
+class whatTheImpress.java {
+
+	public static void main(String[] args) {
+		  System.out.println("*\n**\n***\n****\n*****");
+		
+              /*
+                  *
+                  **
+                  ***
+                  ****
+                  *****
+              */
+	}
+
+}
