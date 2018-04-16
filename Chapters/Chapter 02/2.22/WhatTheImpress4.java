ad6e6cb1a37e26e4747592e5de295398115f83f9
Hianuy <30296650+Hianuy@users.noreply.github.com>
2018-04-16 15:16:35 -0300

    Create WhatTheImpress4.java

---
+++ Chapters/Chapter 02/2.22/WhatTheImpress4.java
@@ -0,0 +1,36 @@
+*
+* 	Filename :WhatTheImpress4.java
+* 
+* 	Descrition : (2.22) What does the following code print?
+*	
+* 			
+*	
+*	Created: 16/04/18 15:16:13
+*
+*	Revision:none
+*	
+*	Author:Hianuy Esperidião - hianuy2@gmail.com
+*	Version 1.0:
+*	
+*	 
+*	 
+* ============================================================================================	 
+*
+
+
+class WhatTheImpress4 {
+
+	public static void main(String[] args) {
+		
+      System.out.print("*");			
+      System.out.println("***");	    // ****
+      System.out.println("*****");	// *****	
+      System.out.print("****");		// ******
+      System.out.println("**");		
+
+		
+		
+		
+	}
+
+}
