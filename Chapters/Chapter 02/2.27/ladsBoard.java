1898cc13e6c8b7bf5fe7cf87f3aa1d13fdfcb0a2
Hianuy <30296650+Hianuy@users.noreply.github.com>
2018-04-18 20:52:44 -0300

    Organization and Cleaning

---
+++ Chapters/Chapter 02/2.27/ladsBoard.java
@@ -0,0 +1,36 @@
+/*
+* 	Filename :  lads_Board.java
+* 
+* 	Descrition : (2.27) Write a program that displays the lady board. Use only asterisks
+
+*	
+* 			
+*	
+*	Created: 18/04/18 20:50:51
+*
+*	Revision:none
+*	
+*	Author:Hianuy Esperidião - hianuy2@gmail.com
+*	Version 1.0:
+*	
+*	 
+*	 
+* ============================================================================================	 
+*/
+
+
+
+class ladsBoard {
+
+    public static void main(String[] args) {
+        System.out.println("* * * * * * * *");
+        System.out.println(" * * * * * * * *");
+        System.out.println("* * * * * * * *");
+        System.out.println(" * * * * * * * *");
+        System.out.println("* * * * * * * *");
+        System.out.println(" * * * * * * * *");
+        System.out.println("* * * * * * * *");
+
+	  }
+
+}
