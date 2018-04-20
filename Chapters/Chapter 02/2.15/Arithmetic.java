c065a06abd00655115af64e86e327047a0d8b713
Hianuy <30296650+Hianuy@users.noreply.github.com>
2018-04-19 22:17:44 -0300

    Discipline and Organization

--- Chapters/Chapter 02/2.15/Arithmetic.java
+++ Chapters/Chapter 02/2.15/Arithmetic.java
@@ -23,42 +23,42 @@ import java.util.*;
 
 public class arithmetic {
       public static void main(String[] args) {
-      Scanner in = new Scanner(System.in);
-		
-      System.out.println("Enter 1 if you want to make a sum:");
-      System.out.println("Enter 2 if you want to make a  subtraction:");
-      System.out.println("Enter 3 if you want to make a multiplication:");
-      System.out.println("Enter 4 if you want to make a  division:");
+	      Scanner in = new Scanner(System.in);
 
-      double i = in.nextInt();
-		
-      System.out.println("Enter  the first number :");
-      double num1 = in.nextInt();
-      System.out.println("Enter the second number :");		
-      double num2 = in.nextInt();
-		
-       if(i==1) {
-	     System.out.println(num1 + num2);
-      }else {
-	   if(i==2) {
-	         System.out.println(num1 - num2);
-		
-		
-	}else
-	     if(i==3) {
-		   System.out.println(num1 * num2);
-	       }
-	else {
-		if(i==4) {
-		      System.out.println(num1/num2);
-       }else {			
-	   System.out.println("Operation invalid, Enter a number in the range of values");
-					
+	      System.out.println("Enter 1 if you want to make a sum:");
+	      System.out.println("Enter 2 if you want to make a  subtraction:");
+	      System.out.println("Enter 3 if you want to make a multiplication:");
+	      System.out.println("Enter 4 if you want to make a  division:");
+
+	      double i = in.nextInt();
+
+	      System.out.println("Enter  the first number :");
+	      double num1 = in.nextInt();
+	      System.out.println("Enter the second number :");		
+	      double num2 = in.nextInt();
+
+	       if(i==1) {
+		     System.out.println(num1 + num2);
+	      }else {
+		   if(i==2) {
+			 System.out.println(num1 - num2);
+
+
+		}else
+		     if(i==3) {
+			   System.out.println(num1 * num2);
+		       }
+		else {
+			if(i==4) {
+			      System.out.println(num1/num2);
+	       }else {			
+		   System.out.println("Operation invalid, Enter a number in the range of values");
+
+			}
+		     }
 		}
-	     }
+	     }		
+
 	}
-     }		
-		
-}
 
 
