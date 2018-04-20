3135da6b8d1539a73d28a25620b2ce95ab61f911
Hianuy <30296650+Hianuy@users.noreply.github.com>
2018-04-19 21:58:53 -0300

    Discipline and Organization

--- Chapters/Chapter 02/2.14/Output_object.java
+++ Chapters/Chapter 02/2.14/Output_object.java
@@ -5,7 +5,7 @@
  			(2.14)Write an application that displays numbers from 1 to 4 on the same line, with each pair of adjacent numbers separated by a space.
 	  		Write the program using the following techniques.
 	
-	Created: 12/04/18
+	Created: 12/04/18 21:20:42
 
 	Revision:none
 	
@@ -19,25 +19,25 @@
 
 
 
+
 class output_object {
-	
-	
-		public static void main(String[] args) {
-/*	1.0 */	  System.out.println("1 2 3 4 ");
+     public static void main(String[] args) {
+	   System.out.println("1 2 3 4 ");
 		
 	    
-/*	1.1 */ 	  System.out.print("1");
-		  System.out.print(" ");
-		  System.out.print("2");
-		  System.out.print(" ");
-		  System.out.print("3");
-		  System.out.print(" ");
-		  System.out.print("4\n");	  
-		  
- /*	1.2 */	  System.out.printf("1 2 3 4" );
+ 	  System.out.print("1");
+	  System.out.print(" ");
+	  System.out.print("2");
+	  System.out.print(" ");
+	  System.out.print("3");
+	  System.out.print(" ");
+	  System.out.print("4\n");	  
+		
+	     
+          System.out.printf("1 2 3 4" );
 		
 	
-		}
+     }
 
 	
 
