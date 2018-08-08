cab9a9e7cc3842e7d0cade89da241e8c2b33cd2d
Hianuy <30296650+Hianuy@users.noreply.github.com>
2018-08-07 22:16:47 -0300

    Programming is pleasurable

---
+++ Chapters/Chapter 03/3.11/GradeBook.java
@@ -0,0 +1,55 @@
+/*
+* 	Filename :  GradeBook.java
+* 
+* 	Descrition : (3.11) modify the Gradebook class figure 3.10
+*	
+* 			
+*	
+*	Created: 07/08/18 22:16:01
+*
+*	Revision:none
+*	
+*	Author:Hianuy Esperidião - hianuy2@gmail.com
+*	Version 1.0:
+*	
+*	 
+*	 
+* ============================================================================================	 
+*/
+
+
+
+public class GradeBook {
+    private String courseName;
+    private String nameInstructor;
+    
+    public  GradeBookTest(String name, String instructor)
+    {
+    courseName = name;
+    nameInstructor = instructor;
+    } 
+   
+    public void setCourseName(String name){
+        courseName = name;
+      
+    }
+    public void setNomeInstrutor(String instructor){
+         nameInstructor = instructor;
+                        
+    } 
+    public String getCourseName(){
+        return courseName;
+        
+    }
+    public String getNameInstructor(){
+        return nameInstructor;
+        
+    }
+    public void displayMessage(){
+        
+     System.out.print("Welcome to the gradebook for "+ getCourseName()); 
+     System.out.printf(".This course is preseted by:%s\n",getNameInstructor());
+    }
+
+
+}
